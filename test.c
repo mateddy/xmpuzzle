4ba1c494dcea9b643bb098cdbf9e652353407973
James Lee <mateddy@gmail.com>
2015-10-05 02:23:15 +0800

    Update from server

--- test.c
+++ test.c
@@ -8,5 +8,5 @@ int main() {
 	int i,j;
 	for (i = 1; i < 10; ++i) for (j =i+1; j < 10; ++j)
 		if (gcd(10*i+j,10*j+i)>5)
-			printf("%d %d  %d\n", 10*i+j, 10*j+i, gcd(10*i+j,10*j+i));
+			printf("%d %d  %d\n", 10*i+j, 10*j+i, gcd(10*i+j,10*j+i)); //meowmeow
 }
